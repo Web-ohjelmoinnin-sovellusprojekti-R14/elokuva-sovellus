41e481dac08e26c06ac092d44217123a2e9b6f62
LASTMARIAN <cheremisin.kirill.2005@gmail.com>
2025-12-21 06:25:16 +0200

    Deleting reviews now updates cache

--- backend/src/routers/deleteReview.js
+++ backend/src/routers/deleteReview.js
@@ -2,12 +2,19 @@ import { Router } from 'express'
 const router = Router()
 import { deleteReviewController } from '../controllers/deleteReviewController.js'
 import { authMe } from '../controllers/authMeController.js'
+import { reviewsCache } from '../controllers/reviewService.js'
 
-router.delete('/delete_review', authMe, async (req, res) => { 
+router.delete('/delete_review', authMe, async (req, res) => {
   try {
     const review_id = req.query.review_id
     const user_id = req.user.user_id
     const response = await deleteReviewController(review_id, user_id, res)
+
+    const cacheKey = `user:${user_id}`
+    if (reviewsCache.has(cacheKey)) {
+      reviewsCache.delete(cacheKey)
+    }
+
     return res.status(201).json(response)
   } catch (err) {
     return res.status(500).json({ error: 'Failed to delete review' })
