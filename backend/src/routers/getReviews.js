e076e1c54fa39d5f1c3a2205099d08b0e284ad8f
LASTMARIAN <cheremisin.kirill.2005@gmail.com>
2026-01-16 01:38:20 +0200

    Added endpoint for getting other users reviews

--- backend/src/routers/getReviews.js
+++ backend/src/routers/getReviews.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getReviewsByUserIdController, getReviewsByMovieIdController } from '../controllers/getReviewsController.js';
+import { getReviewsByUserIdController, getReviewsByMovieIdController } from '../controllers/getReviewsController.js'
 import { authMe } from '../controllers/authMeController.js'
 import { withCache } from '../controllers/cacheWrapper.js'
 import rateLimit from 'express-rate-limit'
@@ -25,24 +25,46 @@ router.get(
   }),
   limiter,
   authMe,
-  async (req, res) => { 
-  try {
-    const user_id = req.user.user_id
-    const response = await getReviewsByUserIdController(user_id)
-    return res.status(201).json(response)
-  } catch (err) {
-    return res.status(500).json({ error: 'Failed to get reviews by User ID' })
+  async (req, res) => {
+    try {
+      const user_id = req.user.user_id
+      const response = await getReviewsByUserIdController(user_id)
+      return res.status(200).json(response)
+    } catch (err) {
+      return res.status(500).json({ error: 'Failed to get reviews by User ID' })
+    }
   }
-})
+)
+
+router.get(
+  '/get_external_reviews_by_user_id',
+  cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    credentials: true,
+  }),
+  limiter,
+  async (req, res) => {
+    try {
+      if (!req.query.user_id) {
+        return res.status(400).json({ error: 'UserID is not provided' })
+      }
+      const user_id = req.query.user_id
+      const response = await getReviewsByUserIdController(user_id)
+      return res.status(200).json(response)
+    } catch (err) {
+      return res.status(500).json({ error: 'Failed to get reviews by User ID' })
+    }
+  }
+)
 
 router.get('/get_reviews_by_movie_id', async (req, res) => {
   try {
-    const { movie_id, media_type } = req.query;
-    const reviews = await getReviewsByMovieIdController(movie_id, media_type);
-    res.json(reviews);
+    const { movie_id, media_type } = req.query
+    const reviews = await getReviewsByMovieIdController(movie_id, media_type)
+    res.json(reviews)
   } catch (err) {
-    console.error(err);
-    res.status(400).json({ error: err.message });
+    console.error(err)
+    res.status(400).json({ error: err.message })
   }
 })
 
